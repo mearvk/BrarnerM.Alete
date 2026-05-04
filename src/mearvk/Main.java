3b4841dba480bab1d4466c4cc384a259875a9a12
mearvk <mearvk@mearvk.us>
2026-05-03 23:02:52 -0400

    Latest...

---
+++ src/mearvk/Main.java
@@ -0,0 +1,13 @@
+package mearvk;
+
+import main.SignalProcessing;
+
+public class Main
+{
+    public static void main(String...args) {}
+
+    public Main()
+    {
+        mearvk.SignalProcessing signal_processing = new mearvk.SignalProcessing();
+    }
+}
